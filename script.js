223035c71dde14baa11bdc22217a52986ff18a78
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-09 18:45:05 -0300

    desenvolvendo a função de fixar notas

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ const addNoteBtn = document.querySelector(".add-note");
 addNoteBtn.addEventListener("click", () => addNote());
 
 function showNotes() {
+  cleanNotes();
+
   getNotes().forEach((note) => {
     const noteElement = createNote(note.id, note.content, note.fixed);
 
     notesContainer.appendChild(noteElement);
   });
+}
+
+function cleanNotes() {
+  notesContainer.replaceChildren([]);
 }
 
 function addNote() {
@@ -49,13 +55,41 @@ function createNote(id, content, fixed) {
 
   element.appendChild(textarea);
 
+  const pinIcon = document.createElement("i");
+
+  pinIcon.classList.add(..."bi", "bi-pin");
+
+  element.appendChild(pinIcon);
+
+  if (fixed) {
+    element.classList.add("fixed");
+  }
+
+  element.querySelector(".bi-pin").addEventListener("click", () => {
+    toggleFixNote(id);
+  });
+
   return element;
+}
+
+function toggleFixNote(id) {
+  const notes = getNotes();
+
+  const targetNote = notes.filter((note) => note.id === id)[0];
+
+  targetNote.fixed = !targetNote.fixed;
+
+  saveNotes(notes);
+
+  showNotes();
 }
 
 function getNotes() {
   const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
-  return notes;
+  const orderedNotes = notes.sort((a, b) => (a.fixed > b.fixed ? -1 : 1));
+
+  return orderedNotes;
 }
 
 function saveNotes(notes) {
